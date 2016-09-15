57bff3661a18b534f0b0f4ba5ed01b059e471b62
Dan Cox <danpcox@gmail.com>
2016-09-15 15:49:30 -0700

    Adding in stat files

---
+++ src/ystats/index.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1" /> 
+<style>
+.alert {background-color:teal;color:white;width:100%;margin-bottom:10px}
+body {padding:10px; font-size:1.2em; margin: auto;}
+@media screen and (min-width: 600px) {
+.container {
+  margin: auto;
+}
+}
+</style>
+</head>
+<body>
+<?
+include_once("Database.php");
+
+$db = Database::getDatabase();
+$todayName = date("l");
+$sql = "select distinct SVP from EMPLOYEE_COUNT order by SVP";
+$result = $db->Query($sql);
+while($row = mysqli_fetch_assoc($result)) {
+  $svp = $row["SVP"];
+  echo "<img src=\"/genStatImage.php?svp=$svp&w=800&h=400\"><br>\n";
+}
+?>
+</body>
+</html>
+
